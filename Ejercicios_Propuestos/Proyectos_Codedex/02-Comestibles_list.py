2d47127c6cb5723e70d47b0b1d5367413f97f63b
Breiner Abello <breiner.abello@cun.edu.co>
2026-04-27 14:50:40 -0500

    feat: add area calculator and grocery todo list programs

---
+++ Ejercicios_Propuestos/Proyectos_Codedex/02-Comestibles_list.py
@@ -0,0 +1,13 @@
+
+grocery = [
+    '🥚 Eggs',
+    '🥑 Avocados',
+    '🍪 Cookies',
+    '🌶 Hot Pepper Jam',
+    '🫐 Blueberries',
+    '🥦 Broccoli',
+]
+
+
+print(grocery)
+
---
+++ Ejercicios_Propuestos/Proyectos_Codedex/03-Todo.py
@@ -0,0 +1,20 @@
+
+
+todo_list = [
+    '🏦 Get quarters.',
+    '🧺 Do laundry.',
+    '🌳 Take a walk.',
+    '💈 Get a haircut.',
+    '🍵 Make some tea.',
+    '💻 Complete Lists chapter.',
+    '💖 Call mom.',
+    '📺 Watch My Hero Academia.'
+]
+
+
+print(todo_list[0:2])
+
+
+print(todo_list[2:5])
+
+#print(todo_list[9])
