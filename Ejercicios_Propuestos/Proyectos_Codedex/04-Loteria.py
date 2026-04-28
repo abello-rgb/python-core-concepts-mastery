343e657173469ba3cf302f9ae2ee843d5c31aa79
Breiner Abello <breiner.abello@cun.edu.co>
2026-04-28 09:14:10 -0500

    feat: add lottery number generator and fortune cookie program

---
+++ Ejercicios_Propuestos/Proyectos_Codedex/04-Loteria.py
@@ -0,0 +1,16 @@
+
+import random
+
+my_numbers = []
+
+winning_numbers = []
+
+for i in range(0,5):
+ my_numbers.append(random.randint(1,69))
+ winning_numbers.append(random.randint(1,69))
+
+my_numbers.append(random.randint(1,26))
+winning_numbers.append(random.randint(1,26))
+
+print(f'My numbers: {my_numbers}')
+print(f'Winning numbers: {winning_numbers}')
